bb5bb5560feb2e43c62299b1f5f40b5658e44e5e
Standard <Standard@Erics-MacBook-Air.local>
2018-06-16 11:23:10 -0400

    June 16, 2018

--- Useful/writingFiles.py
+++ Useful/writingFiles.py
@@ -1,15 +1,21 @@
-FIN="dataout"
+FIN="dataout.txt"
+#If you want to make sure the file is already present
+"""
 try:
    g=open(FIN,'w')
 except:
-   print "ERR: file",FIN,"is not present or can't be opened"
+   raise TypeError("file",FIN,"is not present or can't be opened")
+"""
+
+#The + means if the file does not exist, then python will create it
+g=open(FIN,'w+')
 
 g.write("This is content\n")
 g.write("This is more content\n")
 
 g.close()
 
-#function that replaces a line in a textfule
+#function that replaces a line in a textfile
 def replace_line(file_name, line_num, text):
     lines = open(file_name, 'r').readlines()
     print lines[line_num] #line being replaced
